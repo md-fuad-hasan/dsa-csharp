ca265f230ef12603897a2550a712172dae2e4f61
Fuad Hasan <fuad.ice.ru@gmail.com>
2024-12-08 23:59:36 +0600

    Add project files.

---
+++ DSAPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSAPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
